38e854cfd843ce33385d12999bcb8684c0fb887e
Ronan <ronan.glavin@proton.me>
2025-10-04 15:10:05 -0500

    Pythag start

---
+++ Math/pythag.py
@@ -0,0 +1,16 @@
+import math
+
+def calculate_ab(a,b):
+    return math.sqrt((a ** 2) + (b ** 2))
+
+def calculate_bc(b,c):
+    pass
+
+def calculate_ac(a,c):
+    pass
+
+def calculate_any(a,b,c):
+    '''
+    whichever inputs are -1 are the ones we are looking for
+    '''
+    pass
